2c9d153f172d848a8ebec66f253afe6b33d13046
earl9rey <earl9rey@naver.com>
2025-10-15 20:07:37 +0900

    refactor: Member 필드를 final로 변경하여 불변 객체로 변경

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -4,11 +4,11 @@ import java.time.LocalDate;
 
 public class Member { // Member는 '회원' 한 명을 표현하는 데이터 클래스
 
-    private Long id;   // 회원을 구별하는 고유 번호(자동 증가 등으로 발급)
-    private String name; // 회원 이름
-    private String email;   // 이메일
-    private LocalDate birth;   // 생년월일
-    private Gender gender;  // 성별 (ENUM)
+    private final Long id;   // 회원을 구별하는 고유 번호(자동 증가 등으로 발급)
+    private final String name; // 회원 이름
+    private final String email;   // 이메일
+    private final LocalDate birth;   // 생년월일
+    private final Gender gender;  // 성별 (ENUM)
 
     // 생성자: Member 객체를 만들 때 id와 name을 반드시 받도록 강제
     public Member(Long id, String name, String email, LocalDate birth, Gender gender) {
