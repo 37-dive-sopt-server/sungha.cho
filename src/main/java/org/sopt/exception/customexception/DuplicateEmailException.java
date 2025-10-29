d0637f07d3953e455ab95968d1b2702641b08dca
earl9rey <earl9rey@naver.com>
2025-10-29 21:35:39 +0900

    refactor: 기존 커스텀 예외 클래스 제거

--- src/main/java/org/sopt/exception/customexception/DuplicateEmailException.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.exception.customexception;
-
-import org.sopt.exception.constant.ErrorMessage;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String email) {
-        super(ErrorMessage.DUPLICATE_EMAIL + email);
-    }
-}
--- src/main/java/org/sopt/exception/customexception/EmptyMemberListException.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.exception.customexception;
-
-import org.sopt.exception.constant.ErrorMessage;
-
-public class EmptyMemberListException extends RuntimeException {
-    public EmptyMemberListException() {
-        super(ErrorMessage.EMPTY_MEMBER_LIST);
-    }
-}
