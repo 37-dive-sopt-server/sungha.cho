a268f882daf73b1edaddaff7416b443d0348589d
earl9rey <earl9rey@naver.com>
2025-10-29 21:48:31 +0900

    feat: internal 에러 코드 추가

--- src/main/java/org/sopt/global/exception/constant/ErrorCode.java
+++ src/main/java/org/sopt/global/exception/constant/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
     UNDER_AGE(HttpStatus.BAD_REQUEST, "UNDER_AGE", "⚠️ 가입 불가: 만 20세 미만입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND", "⚠️ 해당 ID의 회원을 찾을 수 없습니다. (ID: {0})"),
     EMPTY_MEMBER_LIST(HttpStatus.NOT_FOUND, "EMPTY_MEMBER_LIST", "ℹ️ 등록된 회원이 없습니다."),
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "⚠️ 내부 오류가 발생했습니다.");
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "⚠️ 내부 오류가 발생했습니다."),
+    FILE_INIT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE_INIT_FAILED", "⚠️ 파일 초기화 중 오류가 발생했습니다."),
+    MEMBER_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MEMBER_SAVE_FAILED", "⚠️ 파일에서 회원 정보를 불러오던 중 오류가 발생했습니다."),
+    FILE_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "FILE_UPDATE_FAILED", "⚠️ 파일 저장 중 오류가 발생했습니다.");
 
     public final HttpStatus status;
     public final String code;
@@ -21,4 +24,3 @@ public enum ErrorCode {
         this.messageTemplate = messageTemplate;
     }
 }
-
