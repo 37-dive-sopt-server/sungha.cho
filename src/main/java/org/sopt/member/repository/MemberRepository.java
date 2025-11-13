d91d9475ab2d89d99f97de83458af8221e2f555a
earl9rey <earl9rey@naver.com>
2025-11-13 13:21:59 +0900

    refactor: 중복된 CRUD 메서드 선언 제거 및 JPA 기본 제공 메서드 활용

--- src/main/java/org/sopt/member/repository/MemberRepository.java
+++ src/main/java/org/sopt/member/repository/MemberRepository.java
@@ -4,14 +4,9 @@ import org.sopt.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member save(Member member);
-    Optional<Member> findById(Long id);
     Optional<Member> findByEmail(String email);
-    List<Member> findAll();
-    void deleteById(Long id);
 }
