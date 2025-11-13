657b742f12ae156bbb6cb3aaa1b0c26f86b947c7
earl9rey <earl9rey@naver.com>
2025-11-13 15:06:39 +0900

    refactor: @RequestMapping 경로에 슬래시(/) 추가

--- src/main/java/org/sopt/article/controller/ArticleController.java
+++ src/main/java/org/sopt/article/controller/ArticleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("articles")
+@RequestMapping("/articles")
 @RequiredArgsConstructor
 public class ArticleController {
 
